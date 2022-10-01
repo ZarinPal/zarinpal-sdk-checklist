83d0633107aa6acc152bb7a8055f230e849ee82b
Masoud Amini <amini141@gmail.com>
2022-10-01 14:57:32 +0330

    Update ZarinPal.php

--- src/ZarinPal.php
+++ src/ZarinPal.php
@@ -14,15 +14,13 @@ use ZarinPal\Sdk\Endpoint\PaymentGateway\PaymentGateway;
 final class ZarinPal
 {
     private ClientBuilder $clientBuilder;
+    private Options $options;
 
-    public function __construct(ClientBuilder $clientBuilder = null, UriFactoryInterface $uriFactory = null)
+    public function __construct(Options $options = null)
     {
-        $this->clientBuilder = $clientBuilder ?: new ClientBuilder();
-        $uriFactory = $uriFactory ?: Psr17FactoryDiscovery::findUriFactory();
-
-        $this->clientBuilder->addPlugin(
-            new BaseUriPlugin($uriFactory->createUri('https://api.zarinpal.com/'))
-        );
+        $this->options = $options ?? new Options();
+        $this->clientBuilder = $this->options->getClientBuilder();
+        $this->clientBuilder->addPlugin(new BaseUriPlugin($this->options->getBaseUrl()));
         $this->clientBuilder->addPlugin(
             new HeaderDefaultsPlugin(
                 [
@@ -37,6 +35,11 @@ final class ZarinPal
     public function paymentGateway(): PaymentGateway
     {
         return new PaymentGateway($this);
+    }
+
+    public function getMerchantId(): string
+    {
+        return $this->options->getMerchantId();
     }
 
     public function getHttpClient(): HttpMethodsClientInterface
