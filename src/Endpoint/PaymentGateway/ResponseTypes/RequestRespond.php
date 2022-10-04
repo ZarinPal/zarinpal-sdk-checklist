c30ef84fe3fc8a15e4908d9426e3cf1fdd7ea70b
Masoud Amini <amini141@gmail.com>
2022-10-04 16:03:40 +0330

    Create RequestRespond.php

---
+++ src/Endpoint/PaymentGateway/ResponseTypes/RequestRespond.php
@@ -0,0 +1,17 @@
+<?php
+namespace ZarinPal\Sdk\Endpoint\PaymentGateway\ResponseTypes;
+
+use ZarinPal\Sdk\Endpoint\Fillable;
+
+class RequestRespond
+{
+
+    use Fillable;
+
+    public string $authority;
+    public int $code;
+    public string $message;
+    public string $fee_type;
+    public int $fee;
+
+}
