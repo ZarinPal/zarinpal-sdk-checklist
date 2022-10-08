f74065ee03f200ab90cb16e1da79d12c592cd23c
Masoud Amini <amini141@gmail.com>
2022-10-08 14:31:23 +0330

    Create UnverifiedRespond.php

---
+++ src/Endpoint/PaymentGateway/ResponseTypes/UnverifiedRespond.php
@@ -0,0 +1,14 @@
+<?php
+namespace ZarinPal\Sdk\Endpoint\PaymentGateway\ResponseTypes;
+
+use ZarinPal\Sdk\Endpoint\Fillable;
+
+class UnverifiedRespond
+{
+
+    use Fillable;
+
+    public array $authorities;
+
+
+}
